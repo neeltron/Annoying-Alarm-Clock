2530495880f8d3995803a17a583eecc86c0cb60e
Neel Adwani <nprosofficial@gmail.com>
2021-07-04 23:13:31 +0000

    Added timer

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from gtts import gTTS
 import os
+from datetime import datetime
 
 text = "wake up bruh"
 language = "en"
 
 speak = gTTS(text = text, lang = language, slow = False)
 speak.save("a.mp3")
-os.system("mpg123 a.mp3")
+
+print(str(datetime.now().time()))
+while True:
+  if str(datetime.now().time()) == '09:00:00':
+    os.system("mpg123 a.mp3")
